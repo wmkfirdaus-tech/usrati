b1c83488837a917b92dc10dd7eb059f2feca373b
wmkfirdaus-tech <wmkfirdaus@gmail.com>
2026-08-10 15:32:26 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='usrati-v1';
+const ASSETS=['./','index.html','style.css','app.js','manifest.json','assets/icon-192.png','assets/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
